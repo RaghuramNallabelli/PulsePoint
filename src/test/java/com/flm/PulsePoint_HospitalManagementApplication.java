10af3ebf8b9353573e9208ebfe8968bca1bafb0b
Raghuram <nallabelliraghuram1999@gmail.com>
2025-10-09 10:19:22 +0530

    file name changed

--- src/test/java/com/flm/PulsePoint_HospitalManagementApplication.java
+++ src/test/java/com/flm/PulsePoint_HospitalManagementApplication.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlmHospitalManagementApplicationTests {
+class PulsePoint_HospitalManagementApplication {
 
 	@Test
 	void contextLoads() {
